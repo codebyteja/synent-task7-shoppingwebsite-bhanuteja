dc08f7aae457611de4f5a56f5f2e98aa69d4a467
codebyteja <chindambhanuteja060@gmail.com>
2026-06-14 18:25:38 +0530

    Added mobile hamburger menu toggle

--- script.js
+++ script.js
@@ -143,3 +143,42 @@ function loadProductDetails() {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Mobile menu toggle
+var bar = document.getElementById('bar');
+var nav = document.getElementById('navbar');
+var close = document.getElementById('close');
+
+if (bar) {
+  bar.addEventListener('click', function() {
+    nav.classList.add('active');
+  });
+}
+
+if (close) {
+  close.addEventListener('click', function() {
+    nav.classList.remove('active');
+  });
+}
